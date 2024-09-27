ab342fe915b242a3a32af99c4302346b1e007653
ItsPapaCKG <grantsputnam@gmail.com>
2024-09-27 17:44:20 +0000

    Added customer database operation commands
    Included exception control for Delete customer

--- AppointmentScheduler/ViewModel/CustomersViewModelProperties.cs
+++ AppointmentScheduler/ViewModel/CustomersViewModelProperties.cs
@@ -1,15 +1,49 @@
-﻿using AppointmentScheduler.Model;
+﻿using AppointmentScheduler.Helpers;
+using AppointmentScheduler.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace AppointmentScheduler.ViewModel
 {
     public partial class MainViewModel
     {
-		private Customer selectedCustomer;
+        public RelayCommand AddCustomer => new(execute => AddCustomerWindow(), canExecute => { return true; });
+        public RelayCommand UpdateCustomer => new(execute => LoadCustomersWindow(), canExecute => { return true; });
+        public RelayCommand DeleteCustomer => new(execute => { }, canExecute => { return SelectedCustomer is not null; });
+
+        public void AddCustomerWindow()
+        {
+            
+        }
+
+        public void UpdateCustomerWindow()
+        {
+
+        } 
+        public void DeleteCustomerWindow() 
+        {
+            try
+            {
+                var existingCustomer = Connection.Customers.FirstOrDefault(c => c.customerId == SelectedCustomer.customerId);
+                if (existingCustomer != null)
+                {
+                    Connection.Customers.Remove(existingCustomer);
+                    Connection.SaveChanges();
+                } else
+                {
+                    throw new Exception("Selected customer does not exist in MySQL database - Customer already absent from system.");
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private Customer selectedCustomer;
 
 		public Customer SelectedCustomer
 		{
